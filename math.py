02a9d0e2e94d167eaaecedb210ededac3503549b
pankhudidhongde <98078188+pankhudidhongde@users.noreply.github.com>
2022-01-20 13:20:09 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,15 @@
+#Add implementation
+def add(x,y):
+    pass
+  
+#Subtract Implementation  
+def subtract(x,y):
+    return x-y  # on master branch
+
+#Multiply implementation
+def multiply(x,y):
+    pass
+  
+#Divide implementation  
+def divide(x,y):
+    pass
